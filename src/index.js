76e5c1b9750274379a46c9f49bee5a2f86260ac8
taylersala <taylersala@gmail.com>
2023-05-26 12:26:18 -0700

    7 min mark

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React { useState } from 'react'
 import { render } from 'react-dom'
 // 👉 App contains a more sophisticated form we'll flesh out later
 import App from './components/App'
@@ -11,8 +11,35 @@ const petsList = [
 ]
 
 function SimpleForm() {
-  return <div>Ready to start GP!</div>
-}
+  const [pets, setPets] = useState(petsList);
+  const [formValues, setFormValues] = useState({ petName="", petType="" })
+  return (
+  <div className="container">
+<h1>Simple Form App</h1>
+{ petsList.map((pet, idx) => {
+  return (
+    <div key="idx">
+      {pet.petName} is a {pet.petType}
+      </div>
+  )
+})}
+<form> 
+  <input 
+  name="petName"
+  type="text"
+  value=""
+  onChange={change}
+  />
+  <input
+   name="petType"
+   type="text"
+   value=""
+   onChange={change}
+   /> 
+
+</form>
+  </div>
+  )}
 
 render(
   <>
